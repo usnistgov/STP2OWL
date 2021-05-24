11bbe5f4a48445197ab044b88b306cebf6c0f168
soonjokwon <67339153+soonjokwon@users.noreply.github.com>
2021-05-25 01:59:06 +0900

    Delete STP2OWL directory

--- STP2OWL/OWL2ProfileEnum.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum class OWL2Profile
-{
-	OWL2_DL,
-	OWL2_EL,
-	OWL2_QL,
-	OWL2_RL
-};
--- STP2OWL/OutputModeEnum.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum class OutputMode
-{
-	Separated,
-	Instance_Only,
-	Schema_Only,
-	Integrated
-};
