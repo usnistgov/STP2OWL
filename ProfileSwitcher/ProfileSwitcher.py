4fc968ab25cd8338f3f97afbbd3b4129b2bbc6d1
Soonjo Kwon <soonjo.kwon.1@gmail.com>
2021-05-27 15:33:14 +0900

    Add files via upload

---
+++ ProfileSwitcher/ProfileSwitcher.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import csv
+import sys
+
+if len(sys.argv) < 3 :
+    logging.error("Usage: ".sys.argv[0] + "[profile] [ontology]")
+
+profile = os.getcwd() + "\\Profile\\" + sys.argv[1]
+ontology = sys.argv[2]
+
+# open file with profile specification
+try :
+    profileFile = open(profile, "r")
+except BaseException as e :
+    logging.error("File ".format(profile) + " couldn't be opened.")
+
+profiles = []
+# read profile specification
+profileReader = csv.reader(profileFile)
+
+for rr in profileReader :
+    p = []
+    for r in rr :
+        try :
+            value = int(r)
+        except BaseException as e:
+            value = r
+        p += [value]
+    profiles += [p]
+
+# put profile specification into a table
+# while profileReader :
+#     profiles.append(profileReader.strip())
+#     profileLine = profileFile.readline()
+
+# profileFile.close()
+
+# with open(profile, newline='') as profileFile :
+#     profiles = csv.DictReader(profileFile)
+
+
+try :
+    ontologyFile = open(ontology, "r")
+except BaseException as e :
+    logging.error("File ".format(ontology) + " couldn't be opened.")
+
+newOntology = ""
+ontologyLine = ontologyFile.readline()
+
+# print(profiles)
+while ontologyLine :
+    ips = 0
+    isOk = 0
+    for ps in profiles :
+        if ips > 0 and ontologyLine.find(ps[0]) >= 0 :
+            # ip = 0
+            for k,p in enumerate(ps) :
+                if p in (0,1) :
+                    if p > 0 and ontologyLine.find(profiles[0][k]) >= 0 :
+                        isOk += 1
+                    # if ps[0].find('int') :
+                        # print("line: " + ontologyLine + " features:" + ps[0] + " " + profiles[0][k] + ":" + str(p) + " isOk=" + str(isOk))
+                        # print(str(p > 0 and ontologyLine.find(profiles[0][k]) >= 0))
+                # ip += 1
+            if isOk <= 0 :
+                newOntology += "# "
+                break
+        ips += 1
+    newOntology += ontologyLine
+    ontologyLine = ontologyFile.readline()
+
+profile = sys.argv[1]
+
+sfn = os.path.splitext(ontologyFile.name)
+newOntologyFileName = sfn[0] + "_" + profile + sfn[1]
+print(newOntologyFileName)
+
+try :
+    newOntologyFile = open(newOntologyFileName, "x")
+except BaseException as e :
+    logging.error("File ".format(newOntologyFileName) + " couldn't be created.")
+
+newOntologyFile.write(newOntology)
+
+
+
---
+++ ProfileSwitcher/README.md
@@ -0,0 +1,16 @@
+# OWL 2 Profile Switcher
+The Profile Switcher is a python code that converts OWL 2 DL into OWLL 2 EL, QL, and RL.
+Please refer to [here](https://www.w3.org/TR/owl2-profiles/) for more details on OWL 2 Profiles.
+
+## Prerequisites
+- Python3.XX needs to be installed first.
+
+## Usage
+- ProfileSwitcher.py [profile] [file]
+
+## Example
+- ProfileSwitcher.py QL test.owl
+  - This will write resulting ontology "test_QL.owl" in the parent folder.
+
+## Contact
+- Laetitia V. Monnier, lve.monnier@gmail.com
